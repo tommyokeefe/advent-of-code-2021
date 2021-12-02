4d8b28d175f37493a45e868728d085fdfe40bb48
Tommy O'Keefe <okeefe@teralogics.com>
2021-12-02 13:21:38 +0000

    AOC day 2

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,7 +1,54 @@
 defmodule AdventOfCode.Day02 do
   def part1(args) do
+    args
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn item -> String.split(item, " ", trim: true) end)
+    |> move_sub([0, 0])
   end
 
   def part2(args) do
+    args
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn item -> String.split(item, " ", trim: true) end)
+    |> move_sub_with_aim([0, 0, 0])
+  end
+
+  def move_sub([["forward" | [steps]] | tail], [distance, depth]) do
+    distance = distance + String.to_integer(steps)
+    move_sub(tail, [distance, depth])
+  end
+
+  def move_sub([["down" | [steps]] | tail], [distance, depth]) do
+    depth = depth + String.to_integer(steps)
+    move_sub(tail, [distance, depth])
+  end
+
+  def move_sub([["up" | [steps]] | tail], [distance, depth]) do
+    depth = depth - String.to_integer(steps)
+    move_sub(tail, [distance, depth])
+  end
+
+  def move_sub([], [distance, depth]) do
+    distance * depth
+  end
+
+  def move_sub_with_aim([["forward" | [steps]] | tail], [distance, depth, aim]) do
+    distance = distance + String.to_integer(steps)
+    depth = depth + aim * String.to_integer(steps)
+    move_sub_with_aim(tail, [distance, depth, aim])
+  end
+
+  def move_sub_with_aim([["down" | [steps]] | tail], [distance, depth, aim]) do
+    aim = aim + String.to_integer(steps)
+    move_sub_with_aim(tail, [distance, depth, aim])
+  end
+
+  def move_sub_with_aim([["up" | [steps]] | tail], [distance, depth, aim]) do
+    aim = aim - String.to_integer(steps)
+    move_sub_with_aim(tail, [distance, depth, aim])
+  end
+
+  def move_sub_with_aim([], [distance, depth, _aim]) do
+    distance * depth
   end
 end
--- test/advent_of_code/day_01_test.exs
+++ test/advent_of_code/day_01_test.exs
@@ -3,6 +3,7 @@ defmodule AdventOfCode.Day01Test do
 
   import AdventOfCode.Day01
 
+  @tag :skip
   test "part1" do
     input = AdventOfCode.Input.get!(1)
 
@@ -11,6 +12,7 @@ defmodule AdventOfCode.Day01Test do
     assert result == 1374
   end
 
+  @tag :skip
   test "part2" do
     input = AdventOfCode.Input.get!(1)
     result = part2(input)
