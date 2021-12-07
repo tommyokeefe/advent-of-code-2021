28a24e6ed42d96661acb33cc084713a083b82fb4
Tommy O'Keefe <okeefe@teralogics.com>
2021-12-07 12:49:17 -0500

    cleaning up day 7 - using reduce

--- lib/advent_of_code_2021/day_07.ex
+++ lib/advent_of_code_2021/day_07.ex
@@ -2,13 +2,13 @@ defmodule AdventOfCode.Day07 do
   def part1(args) do
     crabs = parse(args)
     positions = Enum.to_list(Enum.min(crabs)..Enum.max(crabs))
-    find_most_efficient_position(crabs, positions, :constant_consumption)
+    find_most_efficient_position(crabs, positions, &calculate_constant_consumption/3)
   end
 
   def part2(args) do
     crabs = parse(args)
     positions = Enum.to_list(Enum.min(crabs)..Enum.max(crabs))
-    find_most_efficient_position(crabs, positions, :variable_consumption)
+    find_most_efficient_position(crabs, positions, &calculate_variable_consumption/3)
   end
 
   def parse(data) do
@@ -19,40 +19,19 @@ defmodule AdventOfCode.Day07 do
     |> Enum.map(fn number -> String.to_integer(number) end)
   end
 
-  def find_most_efficient_position(crabs, positions, :constant_consumption) do
-    find_most_efficient_position(crabs, positions, 0, :constant_consumption)
-  end
+  def find_most_efficient_position(crabs, positions, consumption) do
+    Enum.reduce(positions, 0, fn position, minimum_fuel_consumption ->
+      fuel_consumption =
+        Enum.reduce(crabs, 0, fn crab, total ->
+          consumption.(crab, position, total)
+        end)
 
-  def find_most_efficient_position(crabs, positions, :variable_consumption) do
-    find_most_efficient_position(crabs, positions, 0, :variable_consumption)
-  end
-
-  def find_most_efficient_position(crabs, [position | positions], minimum_fuel_used, consumption) do
-    fuel_used =
-      Enum.reduce(crabs, 0, fn crab, total ->
-        cond do
-          consumption == :constant_consumption ->
-            calculate_constant_consumption(crab, position, total)
-
-          consumption == :variable_consumption ->
-            calculate_variable_consumption(crab, position, total)
-        end
-      end)
-
-    cond do
-      minimum_fuel_used == 0 ->
-        find_most_efficient_position(crabs, positions, fuel_used, consumption)
-
-      fuel_used < minimum_fuel_used ->
-        find_most_efficient_position(crabs, positions, fuel_used, consumption)
-
-      true ->
-        find_most_efficient_position(crabs, positions, minimum_fuel_used, consumption)
-    end
-  end
-
-  def find_most_efficient_position(_crabs, [], minimum_fuel_used, _consumption) do
-    minimum_fuel_used
+      cond do
+        minimum_fuel_consumption == 0 -> fuel_consumption
+        fuel_consumption < minimum_fuel_consumption -> fuel_consumption
+        true -> minimum_fuel_consumption
+      end
+    end)
   end
 
   def calculate_constant_consumption(crab, position, total) do
