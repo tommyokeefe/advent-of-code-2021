5819b9804c8f0376c434a3d279957c44ac58a789
Tommy O'Keefe <okeefe@teralogics.com>
2021-12-10 08:15:46 -0500

    day 12 2020 in progress
    Passes tests but fails on part 2 with full data

--- lib/advent_of_code_2020/day_12.ex
+++ lib/advent_of_code_2020/day_12.ex
@@ -1,11 +1,14 @@
 defmodule AdventOfCode2020.Day12 do
   def part1(args) do
-    state = get_original_state()
+    state = get_original_state(:ship)
     input = parse(args)
     drive_ship(input, state)
   end
 
   def part2(args) do
+    state = get_original_state(:waypoint)
+    input = parse(args)
+    drive_ship_with_waypoint(input, state)
   end
 
   def parse(data) do
@@ -18,15 +21,36 @@ defmodule AdventOfCode2020.Day12 do
     end)
   end
 
+  def get_original_state(style) do
+    state = %{
+      north_south: {0, "N"},
+      east_west: {0, "E"},
+      direction: "E",
+      left: ["E", "N", "W", "S", "E", "N", "W", "S"],
+      right: ["E", "S", "W", "N", "E", "S", "W", "N"]
+    }
+
+    case style do
+      :ship ->
+        state
+
+      :waypoint ->
+        Map.put(state, :north_south_ship, {0, "N"})
+        |> Map.put(:east_west_ship, {0, "E"})
+        |> Map.put(:north_south, {1, "N"})
+        |> Map.put(:east_west, {10, "E"})
+    end
+  end
+
   def drive_ship([{direction, value} | input], state) do
     case direction do
       "N" -> drive_ship(input, move_north(value, state))
       "S" -> drive_ship(input, move_south(value, state))
       "E" -> drive_ship(input, move_east(value, state))
       "W" -> drive_ship(input, move_west(value, state))
-      "L" -> drive_ship(input, turn_left(value, state))
-      "R" -> drive_ship(input, turn_right(value, state))
-      "F" -> drive_ship(input, go_forward(value, state))
+      "L" -> drive_ship(input, turn_left(value, state, :ship))
+      "R" -> drive_ship(input, turn_right(value, state, :ship))
+      "F" -> drive_ship(input, go_forward(value, state, :ship))
     end
   end
 
@@ -36,14 +60,28 @@ defmodule AdventOfCode2020.Day12 do
     north_south + east_west
   end
 
-  def get_original_state do
-    %{
-      north_south: {0, "N"},
-      east_west: {0, "E"},
-      direction: "E",
-      left: ["E", "N", "W", "S", "E", "N", "W", "S"],
-      right: ["E", "S", "W", "N", "E", "S", "W", "N"]
-    }
+  def drive_ship_with_waypoint([{direction, value} | input], state) do
+    IO.puts("\nCurrent State:")
+    IO.inspect(state)
+    IO.puts("Next direction: #{direction} #{value}")
+
+    case direction do
+      "N" -> drive_ship_with_waypoint(input, move_north(value, state))
+      "S" -> drive_ship_with_waypoint(input, move_south(value, state))
+      "E" -> drive_ship_with_waypoint(input, move_east(value, state))
+      "W" -> drive_ship_with_waypoint(input, move_west(value, state))
+      "L" -> drive_ship_with_waypoint(input, turn_left(value, state, :waypoint))
+      "R" -> drive_ship_with_waypoint(input, turn_right(value, state, :waypoint))
+      "F" -> drive_ship_with_waypoint(input, go_forward(value, state, :waypoint))
+    end
+  end
+
+  def drive_ship_with_waypoint([], state) do
+    IO.puts("\nCurrent State:")
+    IO.inspect(state)
+    {north_south, _} = Map.get(state, :north_south_ship)
+    {east_west, _} = Map.get(state, :east_west_ship)
+    north_south + east_west
   end
 
   def move_north(distance, state) do
@@ -106,21 +144,52 @@ defmodule AdventOfCode2020.Day12 do
     end
   end
 
-  def turn_left(degrees, state) do
+  def turn_left(degrees, state, :ship) do
     direction = Map.get(state, :direction)
     heading = Map.get(state, :left)
     index = Enum.find_index(heading, fn item -> item == direction end) + div(degrees, 90)
     Map.put(state, :direction, Enum.at(heading, index))
   end
 
-  def turn_right(degrees, state) do
+  def turn_left(degrees, state, :waypoint) do
+    heading = Map.get(state, :left)
+    rotate(state, heading, degrees)
+  end
+
+  def turn_right(degrees, state, :ship) do
     direction = Map.get(state, :direction)
     heading = Map.get(state, :right)
     index = Enum.find_index(heading, fn item -> item == direction end) + div(degrees, 90)
     Map.put(state, :direction, Enum.at(heading, index))
   end
 
-  def go_forward(distance, state) do
+  def turn_right(degrees, state, :waypoint) do
+    heading = Map.get(state, :right)
+    rotate(state, heading, degrees)
+  end
+
+  def rotate(state, heading, degrees) do
+    {distance1, bearing1} = Map.get(state, :north_south)
+    {distance2, bearing2} = Map.get(state, :east_west)
+
+    bearing1 =
+      Enum.at(
+        heading,
+        Enum.find_index(heading, fn item -> item == bearing1 end) + div(degrees, 90)
+      )
+
+    bearing2 =
+      Enum.at(
+        heading,
+        Enum.find_index(heading, fn item -> item == bearing2 end) + div(degrees, 90)
+      )
+
+    state
+    |> update_bearing(distance1, bearing1)
+    |> update_bearing(distance2, bearing2)
+  end
+
+  def go_forward(distance, state, :ship) do
     direction = Map.get(state, :direction)
 
     case direction do
@@ -129,5 +198,45 @@ defmodule AdventOfCode2020.Day12 do
       "E" -> move_east(distance, state)
       "W" -> move_west(distance, state)
     end
+  end
+
+  def go_forward(multiplier, state, :waypoint) do
+    {distance1, bearing1} = Map.get(state, :north_south)
+    {distance2, bearing2} = Map.get(state, :east_west)
+
+    Map.update(state, :north_south_ship, {0, ""}, fn
+      {value, ^bearing1} ->
+        {distance1 * multiplier + value, bearing1}
+
+      {value, new_bearing} ->
+        cond do
+          value - distance1 * multiplier > 0 ->
+            {value - distance1 * multiplier, bearing1}
+
+          true ->
+            {abs(value - distance1 * multiplier), new_bearing}
+        end
+    end)
+    |> Map.update(:east_west_ship, {0, ""}, fn
+      {value, ^bearing2} ->
+        {distance2 * multiplier + value, bearing2}
+
+      {value, new_bearing} ->
+        cond do
+          value - distance2 * multiplier > 0 ->
+            {value - distance2 * multiplier, bearing2}
+
+          true ->
+            {abs(value - distance2 * multiplier), new_bearing}
+        end
+    end)
+  end
+
+  def update_bearing(state, distance, bearing) when bearing == "E" or bearing == "W" do
+    Map.put(state, :east_west, {distance, bearing})
+  end
+
+  def update_bearing(state, distance, bearing) when bearing == "N" or bearing == "S" do
+    Map.put(state, :north_south, {distance, bearing})
   end
 end
--- test/advent_of_code_2020/day_12_test.exs
+++ test/advent_of_code_2020/day_12_test.exs
@@ -18,11 +18,35 @@ defmodule AdventOfCode2020.Day12Test do
     assert result == 25
   end
 
-  @tag :skip
+  # @tag :skip
   test "part2" do
-    input = nil
+    input = """
+    F10
+    N3
+    F7
+    R90
+    F11
+    """
+
     result = part2(input)
 
-    assert result
+    assert result == 286
+  end
+
+  test "move_north" do
+    state = %{
+      direction: "E",
+      east_west: {4, "E"},
+      east_west_ship: {214, "E"},
+      left: ["E", "N", "W", "S", "E", "N", "W", "S"],
+      north_south: {10, "S"},
+      north_south_ship: {72, "N"},
+      right: ["E", "S", "W", "N", "E", "S", "W", "N"]
+    }
+
+    result = move_north(20, state)
+    north_south = Map.get(result, :north_south)
+
+    assert north_south == {10, "N"}
   end
 end
