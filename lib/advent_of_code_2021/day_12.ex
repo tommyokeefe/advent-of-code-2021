9f89bd9304bf44d701a5351f08e953ddd14ed73e
Tommy O'Keefe <okeefe@teralogics.com>
2021-12-12 09:56:39 -0500

    day 12

--- lib/advent_of_code_2021/day_12.ex
+++ lib/advent_of_code_2021/day_12.ex
@@ -1,7 +1,47 @@
 defmodule AdventOfCode.Day12 do
   def part1(args) do
+    parse(args)
+    |> count_paths()
   end
 
   def part2(args) do
+    parse(args)
+    |> count_paths(false)
+  end
+
+  def parse(data) do
+    String.split(data, "\n", trim: true)
+    |> Enum.reduce(%{}, fn line, connections ->
+      [from | [to]] = String.split(line, "-", trim: true)
+
+      Map.update(connections, from, [to], fn destinations -> [to | destinations] end)
+      |> Map.update(to, [from], fn destinations -> [from | destinations] end)
+    end)
+  end
+
+  def count_paths(caves, limit_visits? \\ true, current_cave \\ "start", visited \\ MapSet.new())
+
+  def count_paths(_, _, "end", _) do
+    1
+  end
+
+  def count_paths(caves, limit_visits?, current_cave, visited) do
+    waypoints = Map.get(caves, current_cave)
+
+    waypoints
+    |> Enum.reject(&(&1 == "start"))
+    |> Enum.map(fn waypoint ->
+      cond do
+        waypoint == String.downcase(waypoint) and waypoint in visited and limit_visits? ->
+          0
+
+        waypoint == String.downcase(waypoint) and waypoint in visited ->
+          count_paths(caves, true, waypoint, MapSet.put(visited, waypoint))
+
+        true ->
+          count_paths(caves, limit_visits?, waypoint, MapSet.put(visited, waypoint))
+      end
+    end)
+    |> Enum.sum()
   end
 end
--- test/advent_of_code_2021/day_12_test.exs
+++ test/advent_of_code_2021/day_12_test.exs
@@ -5,17 +5,35 @@ defmodule AdventOfCode.Day12Test do
 
   @tag :skip
   test "part1" do
-    input = nil
+    input = """
+    start-A
+    start-b
+    A-c
+    A-b
+    b-d
+    A-end
+    b-end
+    """
+
     result = part1(input)
 
-    assert result
+    assert result == 10
   end
 
-  @tag :skip
+  # @tag :skip
   test "part2" do
-    input = nil
+    input = """
+    start-A
+    start-b
+    A-c
+    A-b
+    b-d
+    A-end
+    b-end
+    """
+
     result = part2(input)
 
-    assert result
+    assert result == 36
   end
 end
