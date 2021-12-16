584f065dd76a2cc0be9d03452ff4a8aa2e12792f
Tommy O'Keefe <okeefe@teralogics.com>
2021-12-15 22:31:57 -0500

    Day 15
    After doing a bit of reading I implemented a Dijkstra algorithm and learned about .
    Struggled with the enlarge bit until i looked at some other code that did similar things.

--- lib/advent_of_code_2021/day_15.ex
+++ lib/advent_of_code_2021/day_15.ex
@@ -1,7 +1,92 @@
 defmodule AdventOfCode.Day15 do
   def part1(args) do
+    grid = parse(args)
+    find_shortest_path(grid)
   end
 
   def part2(args) do
+    grid = parse(args)
+    {{x, y}, _} = Enum.max(grid)
+    grid = enlarge(grid, x, y)
+    find_shortest_path(grid)
+  end
+
+  def find_shortest_path(grid) do
+    {start, _} = Enum.min(grid)
+    {destination, _} = Enum.max(grid)
+    next_steps = :gb_sets.singleton({0, start})
+    visited = MapSet.new()
+    find_shortest_path(grid, start, destination, next_steps, visited)
+  end
+
+  def find_shortest_path(grid, start, destination, next_steps, visited) do
+    {{distance, point}, next_steps} = :gb_sets.take_smallest(next_steps)
+
+    case point do
+      ^destination ->
+        distance
+
+      _ ->
+        visited = MapSet.put(visited, point)
+        neighbors = get_neighbors(point, destination)
+
+        next_steps =
+          Enum.reduce(neighbors, next_steps, fn neighbor, next_steps ->
+            cond do
+              MapSet.member?(visited, neighbor) ->
+                next_steps
+
+              true ->
+                priority = grid[neighbor] + distance
+                :gb_sets.add_element({priority, neighbor}, next_steps)
+            end
+          end)
+
+        find_shortest_path(grid, start, destination, next_steps, visited)
+    end
+  end
+
+  def get_neighbors({x, y}, destination) do
+    adjacent_deltas = [{0, -1}, {-1, 0}, {0, 1}, {1, 0}]
+    {max_x, max_y} = destination
+
+    for {delta_x, delta_y} <- adjacent_deltas,
+        {new_x, new_y} = {x + delta_x, y + delta_y},
+        new_x >= 0 and new_y >= 0 and new_x <= max_x and new_y <= max_y,
+        do: {new_x, new_y}
+  end
+
+  def enlarge(map, max_x, max_y) do
+    for x_factor <- 0..4,
+        y_factor <- 0..4,
+        reduce: map do
+      new_map ->
+        Map.merge(new_map, enlarged(map, x_factor, y_factor, max_x, max_y))
+    end
+  end
+
+  defp enlarged(map, x_factor, y_factor, max_x, max_y) do
+    for {{x, y}, risk} <- map, into: %{} do
+      new_x = x + x_factor * (max_x + 1)
+      new_y = y + y_factor * (max_y + 1)
+      new_risk = risk + x_factor + y_factor
+      new_risk = if new_risk <= 9, do: new_risk, else: new_risk - 9
+
+      {{new_x, new_y}, new_risk}
+    end
+  end
+
+  def parse(data) do
+    String.split(data, "\n", trim: true)
+    |> Enum.map(fn line ->
+      String.to_charlist(line)
+      |> Enum.map(&(&1 - ?0))
+    end)
+    |> Enum.with_index()
+    |> Enum.flat_map(fn {line, row} ->
+      Enum.with_index(line)
+      |> Enum.map(fn {point, column} -> {{row, column}, point} end)
+    end)
+    |> Map.new()
   end
 end
--- test/advent_of_code_2021/day_15_test.exs
+++ test/advent_of_code_2021/day_15_test.exs
@@ -5,17 +5,41 @@ defmodule AdventOfCode.Day15Test do
 
   @tag :skip
   test "part1" do
-    input = nil
+    input = """
+    1163751742
+    1381373672
+    2136511328
+    3694931569
+    7463417111
+    1319128137
+    1359912421
+    3125421639
+    1293138521
+    2311944581
+    """
+
     result = part1(input)
 
-    assert result
+    assert result == 40
   end
 
   @tag :skip
   test "part2" do
-    input = nil
+    input = """
+    1163751742
+    1381373672
+    2136511328
+    3694931569
+    7463417111
+    1319128137
+    1359912421
+    3125421639
+    1293138521
+    2311944581
+    """
+
     result = part2(input)
 
-    assert result
+    assert result == 315
   end
 end
